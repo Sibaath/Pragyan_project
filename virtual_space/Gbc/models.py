538612fadef6a2ab62698b82a63d0f6e9c56aed7
Sibaath <sibaathahmed2004@gmail.com>
2024-02-16 21:05:57 +0530

    Basic project completed

--- virtual_space/Gbc/models.py
+++ virtual_space/Gbc/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+class User(models.Model):
+    api_token = models.CharField(max_length=255,unique=True)
+    
 # Create your models here.
--- virtual_space/Gbc/views.py
+++ virtual_space/Gbc/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_list_or_404
+from .models import User
+ 
+
 
 def say_hello(request):
     # return HttpResponse("Hello world")
-    x=1
-    y=1
+    if request.method == 'POST':
+        api_token = request.POST.get('api_token')
+        if api_token:
+            
+            
     return render(request,'index.html',{ 'name' : 'Sibaath'})
 
 # Create your views here.
